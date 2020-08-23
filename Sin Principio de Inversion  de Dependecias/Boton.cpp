72daa4f59490cc60a9dca821d1393c37c0e5a48f
steveMadrigal <68918625+steveMadrigal@users.noreply.github.com>
2020-08-23 16:18:16 -0600

    Editado v4.00

--- Sin Principio de Inversion  de Dependecias/Boton.cpp
+++ Sin Principio de Inversion  de Dependecias/Boton.cpp
@@ -1,7 +1,15 @@
-//
-// Created by computer on 8/21/2020.
-//
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/* 
+ * File:   Boton.cpp
+ * Author: computer
+ * 
+ * Created on August 6, 2020, 9:39 AM
+ */
 
 #include "Boton.h"
 
@@ -10,6 +18,7 @@ Boton::Boton() {//Constructor
 
 
 Boton::~Boton() {//Destructor
+    delete _lampara;//Liberacion de la memoria
 }
 
 void Boton::presionar(bool s){//Metodo void con decision
